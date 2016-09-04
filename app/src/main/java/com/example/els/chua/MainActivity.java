68fd73a5ed01aaab1300d0b52888756ff41ef461
Jesimiel Rufino <jesrufino@gmail.com>
2016-09-03 21:39:41 -0300

    app basketball
    Arquivos do app que controla o placaar do jogo de basketball

---
+++ app/src/main/java/com/example/els/chua/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.els.chua;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    int pontos = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void cestaDeTres(View v) {
+        pontos = pontos + 3;
+        displayForTeamA(pontos);
+    }
+
+    public void cestaDeDois(View v) {
+        pontos = pontos + 2;
+        displayForTeamA(pontos);
+    }
+
+    public void TiroLivre(View v) {
+        pontos = pontos + 1;
+        displayForTeamA(pontos);
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
